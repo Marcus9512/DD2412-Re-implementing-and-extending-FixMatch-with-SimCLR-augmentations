727e832f0a8c2342348031a11638d5a74d690538
Cory Trevor <smallgreycreatures@gmail.com>
2020-10-09 11:14:24 +0200

    loss function, weight decay and fixed bug in unlabeled data

--- Custom_dataset/Labeled_Unlabeled_dataset.py
+++ Custom_dataset/Labeled_Unlabeled_dataset.py
@@ -17,6 +17,6 @@ class L_U_Dataset(Dataset):
     def __getitem__(self, item):
         index_pos_2 = item * self.mu
         labeled_data = self.labeled[item]
-        unlabeled_data = torch.cat(tuple(self.unlabeled[i][0] for i in range(index_pos_2, index_pos_2 + self.mu)))
+        unlabeled_data = tuple(self.unlabeled[i][0] for i in range(index_pos_2, index_pos_2 + self.mu))
 
         return labeled_data, unlabeled_data
