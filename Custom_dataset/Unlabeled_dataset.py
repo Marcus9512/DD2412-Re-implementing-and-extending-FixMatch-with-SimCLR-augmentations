Normalization fix

Custom_dataset/Unlabeled_dataset.py
@@ -14,6 +14,8 @@ These classes acts as extension to CIFAR-10 and CIFAR-100 to enable both weak an
 class Unlabeled_dataset_cifar100(torchvision.datasets.CIFAR100):
     '''
     Custom dataset designed to return a 2 transformations of the loaded image for CIFAR-100
+
+    Influenced by: https://pytorch.org/docs/stable/_modules/torchvision/datasets/cifar.html#CIFAR100
     '''
     def __init__(self, root, train, transform, data_indicies):
         super().__init__(root=root, train=train, download=False, transform=transform)
@@ -21,9 +23,19 @@ class Unlabeled_dataset_cifar100(torchvision.datasets.CIFAR100):
         self.targets = np.array(self.targets)[data_indicies]
 
     def __getitem__(self, item):
-        datasample = p.Image.fromarray(self.data[item])
-        target = self.targets[item]
-        return self.transform(datasample), self.target_transform(target)
+        img, target = self.data[item], self.targets[item]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = p.Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
 
 class Unlabeled_dataset_cifar10(torchvision.datasets.CIFAR10):
     '''
@@ -35,6 +47,8 @@ class Unlabeled_dataset_cifar10(torchvision.datasets.CIFAR10):
         super().__init__(root=root, train=train, download=download, transform=transform)
         self.data = self.data[data_indicies]
         self.targets = np.array(self.targets)[data_indicies]
+
+
 
     def __getitem__(self, item):
         img, target = self.data[item], self.targets[item]

Trainer.py
@@ -115,6 +115,7 @@ class Trainer:
         labeled = np.append(labeled, expand_label)
         unlabeled = np.append(unlabeled, expand_unlabel)
 
+
         return labeled, unlabeled
 
 
@@ -328,6 +329,12 @@ class Trainer:
                         batch_X, label_X = X
                         (weak_a, strong_a), _ = U
 
+                        label_X = label_X.long()
+                        #print(label_X)
+                        #print(len(label_X))
+                        #print(label_X.shape)
+                        #print(label_X.long())
+
                         #print(weak_a.shape)
                         #print(strong_a.shape)
                         #print("Label ",label_X)
@@ -340,7 +347,6 @@ class Trainer:
                         # Verification have no unlabeled dataset
                         batch_X, label_X = (X, U)
 
-                    print("Here ",j)
 
                     # Send sample and label to GPU or CPU
                     batch_X = batch_X.to(device=self.main_device)
@@ -413,8 +419,7 @@ class Trainer:
                         optimizer.step()
                         scheduler.step()
                         self.ema.update(model.parameters())
-                    if (i % 1000 == 0):
-                        self.logger.info(f"{session} img: {i}")
+
 
                     i += label_X.size(0)
                     pbar.update(1)